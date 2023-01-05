87ea6a1a687451d609b731f2c7d4c3a1f9e89b34
gabriel <gabrieuvelezmoro@gmail.com>
2023-01-05 13:25:13 -0300

    feat: mostra detalhes da postagem

--- src/components/BlogItem/index.tsx
+++ src/components/BlogItem/index.tsx
@@ -10,10 +10,10 @@ const BlogItem: React.FC = () => {
       <StyledH2><StyledA
        href="/hello-world/">Hello World</StyledA></StyledH2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione obcaecati veritatis vel. Deleniti explicabo dolores voluptates pariatur sit quos odit. Quod error id ad alias voluptate asperiores commodi harum perferendis.</p>
-      <p className="meta">
+      <p >
         <img src="" alt="" />
-        <i className='link-spacer'></i>
-        <time className='post-date' dateTime='2023-01-05'> </time>
+        <i className='link-spacer'>Gabriel Velezmoro</i>
+        <time className='post-date' dateTime='2023-01-05'> 2023-01-05 </time>
       </p>
     </StyledCounter>
   </article> 
