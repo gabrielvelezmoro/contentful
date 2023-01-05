2b81ff8c23d01a576536c9bcec4809f5db16e67c
gabriel <gabrieuvelezmoro@gmail.com>
2023-01-05 13:47:56 -0300

    feat: pegando dados do contentful e logando no console

--- src/components/BlogList/index.tsx
+++ src/components/BlogList/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState}from 'react';
 import BlogItem from '../BlogItem';
 import {StyledCounter} from './styles';
-
+var contentful = require('contentful')
 
 
 const BlogList: React.FC = () => {
+
+  const [data, setData] = useState<any>();
+  
   const posts = []
+
+  const connectContentful = async () => {
+      const client = contentful.createClient({
+       space: "srzlwc3or4mg",
+       accessToken: "0AmslYmvltUHQH23nNPUwF6mqk5SyuGPG2Zs646oYr0"
+     });
+
+     console.log('tentei pegar os conteudos')
+     
+     client?.getEntries().then((response: any) => {
+       console.log('resposta: ', response.items)
+       setData(response.items )
+      })
+  
+  }
+
+  useEffect(() => {
+     connectContentful()
+  }, []);
+
   for (let i=0; i<6; i++){
       posts.push(<BlogItem  key={i}/>)
   }
